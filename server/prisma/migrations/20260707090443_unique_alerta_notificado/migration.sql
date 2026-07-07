6247e733e6546fbb790533786fe5f89efed377ef
JoaoGabriel04 <jgabrielcastro04@gmail.com>
2026-07-07 09:05:08 -0300

    fix(server): enforce unique constraint on (medicamentoId, tipo) in alertas_notificados

---
+++ server/prisma/migrations/20260707090443_unique_alerta_notificado/migration.sql
@@ -0,0 +1,2 @@
+﻿-- AlterTable
+ALTER TABLE "alertas_notificados" DROP CONSTRAINT "alertas_notificados_medicamento_id_tipo_key", ADD CONSTRAINT "alertas_notificados_medicamento_id_tipo_key" UNIQUE ("medicamento_id", "tipo");
